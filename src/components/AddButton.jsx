b7f421120dbed58dfb28dbb78a60c47172c80c20
Ankit Ydv <ankityadav11241@gmail.com>
2024-06-15 11:33:09 +0530

    fixed position

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 export default function AddButton() {
   return (
-    <NavLink to={"/add"} className="absolute bottom-6 right-6 text-xl bg-yellow-400 text-gray-800 p-4 rounded-full flex items-center justify-center cursor-pointer">
+    <NavLink to={"/add"} className="fixed bottom-6 right-6 text-xl bg-yellow-400 text-gray-800 p-4 rounded-full flex items-center justify-center cursor-pointer">
       <span className="text-xl mx-2">+</span>
       <span>New</span>
     </NavLink>
